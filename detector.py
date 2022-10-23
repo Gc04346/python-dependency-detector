dccb100f1ec50c27b6c391920509f3307e31bbc4
Gc04346 <gc04346@gmail.com>
2022-10-23 12:44:37 -0300

    mais analises graficas

--- detector.py
+++ detector.py
@@ -1,39 +1,34 @@
 import os
 
-from output_gen import generate_html_output
+from output_gen import generate_html_output, generate_total_graphic, generate_double_graphic, \
+    generate_different_components_called_graphic, generate_different_components_received_graphic
 
 BASE_DIR = 'input_files'
 METHOD_PREFIX = 'def '
 
-components = sorted({directory for directory in os.listdir(BASE_DIR)})
+components = sorted({directory for directory in next(os.walk(BASE_DIR))[1]})
 methods = set()
 method_components_relation = {}
 for component in components:
-    # print(component.upper())
     current_dir = os.path.join(BASE_DIR, component)
     for file in os.listdir(current_dir):
         filepath = os.path.join(current_dir, file)
-        # print(file)
         with open(filepath) as f:
             while line := f.readline():
                 if METHOD_PREFIX in line:
-                    method = line.strip(METHOD_PREFIX).split('(')[0]+'('
+                    method = line.strip(METHOD_PREFIX).split('(')[0] + '('
                     methods.add(method)
                     method_components_relation[method] = component
-    #     print('\n')
-    # print('\n')
 
-# print(method_components_relation)
 components_deps = {}
+zeros_list = [0 for i in enumerate(components)]
 
 for component in components:
-    components_deps[component] = [0 for i in enumerate(components)]
+    components_deps[component] = zeros_list.copy()
     component_deps = components_deps[component]
-    # print(component.upper())
     current_dir = os.path.join(BASE_DIR, component)
     for file in os.listdir(current_dir):
         filepath = os.path.join(current_dir, file)
-        # print(file)
         with open(filepath) as f:
             while line := f.readline():
                 for method in methods:
@@ -42,3 +37,41 @@ for component in components:
 dep_matrix = [deps for deps in components_deps.values()]
 
 generate_html_output(components, dep_matrix)
+
+dependency_sums = zeros_list.copy()
+calls_made = zeros_list.copy()
+calls_received = zeros_list.copy()
+different_components_called = zeros_list.copy()
+different_components_received = zeros_list.copy()
+
+for i, row in enumerate(dep_matrix):  # Percorrendo as linhas da matriz
+    calls_made[i] = sum(row)
+    different_components_called[i] = sum(1 for val in row if val)
+    for j, val in enumerate(row):  # Percorrendo cada linha
+        if i == j:  # Não soma nos casos onde o componente chama a si mesmo
+            calls_made[i] -= val
+            continue
+        dependency_sums[i] += val
+        dependency_sums[j] += val
+        calls_received[j] += val
+        if val:
+            different_components_received[j] += 1
+components = [component.replace(' ', '\n') for component in components]
+ordered_total_calls = sorted(list(zip(components, dependency_sums)), key=lambda x: x[1])
+ordered_calls_by_received = sorted(list(zip(components, calls_made, calls_received)), key=lambda x: x[2])
+ordered_different_components_called = sorted(list(zip(components, different_components_called)), key=lambda x: x[1])
+ordered_different_components_received = sorted(list(zip(components, different_components_received)), key=lambda x: x[1])
+
+generate_total_graphic(
+    [component for component, total_calls in ordered_total_calls],
+    [total_calls for component, total_calls in ordered_total_calls])
+generate_double_graphic(
+    [component for component, calls_made, calls_received in ordered_calls_by_received],
+    [calls_made for component, calls_made, calls_received in ordered_calls_by_received],
+    [calls_received for component, calls_made, calls_received in ordered_calls_by_received])
+generate_different_components_called_graphic(
+    [component for component, total_calls in ordered_different_components_called],
+    [total_calls for component, total_calls in ordered_different_components_called])
+generate_different_components_received_graphic(
+    [component for component, total_calls in ordered_different_components_received],
+    [total_calls for component, total_calls in ordered_different_components_received])
--- output_gen.py
+++ output_gen.py
@@ -11,7 +11,7 @@ def generate_html_output(modules, dep_matrix):
         <h3>Módulos identificados: {}</h3>
         <table>
             <tr style="text-align: center">
-                <th></th>
+                <th>\/Chama/É Chamado ></th>
     """.format(', '.join(modules))
     output_file.write(prefix)
     for module in modules:
@@ -29,3 +29,55 @@ def generate_html_output(modules, dep_matrix):
     </html>
     """)
     output_file.close()
+
+def generate_total_graphic(xAxis, yAxis):
+    import matplotlib.pyplot as plt
+    bc = plt.bar(xAxis, yAxis)
+    plt.bar_label(bc, yAxis)
+    plt.title('Nível de Acoplamento dos Componentes')
+    plt.xlabel('Componente')
+    plt.ylabel('N° de Chamadas Feitas e Recebidas de Outros Componentes')
+    plt.show()
+    plt.savefig('output/chamadas_totais.png')
+
+
+def generate_different_components_called_graphic(xAxis, yAxis):
+    import matplotlib.pyplot as plt
+    bc = plt.bar(xAxis, yAxis, label=yAxis)
+    plt.bar_label(bc, yAxis)
+    plt.title('Número de interações com outros componentes')
+    plt.xlabel('Componente')
+    plt.ylabel('Chamadas feitas a outros componentes')
+    plt.show()
+    plt.savefig('output/comp_diferentes_chamados.png')
+
+
+def generate_different_components_received_graphic(xAxis, yAxis):
+    import matplotlib.pyplot as plt
+    bc = plt.bar(xAxis, yAxis)
+    plt.bar_label(bc, yAxis)
+    plt.title('Número de componentes dependentes')
+    plt.xlabel('Componente')
+    plt.ylabel('Chamadas recebidas de outros componentes')
+    plt.show()
+    plt.savefig('output/comp_diferentes_atendidos.png')
+
+def generate_double_graphic(xAxis, calls_made, calls_received):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    width = 0.35  # the width of the bars
+    x = np.arange(len(xAxis))
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width / 2, calls_made, width, label='Chamadas Feitas')
+    rects2 = ax.bar(x + width / 2, calls_received, width, label='Chamadas Recebidas')
+    ax.set_xlabel('Componente')
+    ax.set_ylabel('Chamadas')
+    ax.set_title('N° de Chamadas Feitas/Recebidas de Outros Componentes')
+    ax.set_xticks(x, xAxis)
+    ax.legend()
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+
+    fig.tight_layout()
+    plt.show()
+    plt.savefig('output/feitas_x_recebidas.png')
